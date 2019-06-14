d47e816dbd4c835eac05cbad964ee652fe8f3977
Dag-Erling Smørgrav <des@des.no>
2019-06-14 15:36:08 +0200

    Complete 2015-04.

---
+++ 2015/04/two.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func main() {
+	var key string
+	fmt.Scan(&key)
+	var i int
+	for i = 1; true; i++ {
+		block := fmt.Sprintf("%s%d", key, i)
+		md5 := md5.Sum([]byte(block))
+		if md5[0] == 0 && md5[1] == 0 && md5[2] == 0 {
+			break
+		}
+	}
+	fmt.Println(i)
+}
