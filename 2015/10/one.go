fc20d619313c943426e87da1e5a76b626979c0d2
Dag-Erling Smørgrav <des@des.no>
2019-06-22 15:32:59 +0200

    Complete 2015-10.

---
+++ 2015/10/one.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func look_say(input string) (output string) {
+	var prev rune
+	var count uint
+	for _, char := range input + "@" {
+		switch {
+		case prev == 0:
+			prev = char
+			count = 1
+		case prev == char:
+			count++
+		default:
+			output += fmt.Sprintf("%d%c", count, prev)
+			prev = char
+			count = 1
+		}
+	}
+	return
+}
+
+func main() {
+	var sequence string
+	fmt.Scan(&sequence)
+	for i := 1; i <= 40; i++ {
+		// fmt.Printf("%d ", i)
+		sequence = look_say(sequence)
+		// fmt.Printf("%d\n", len(sequence))
+	}
+	fmt.Println(len(sequence))
+}
